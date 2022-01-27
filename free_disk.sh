498b433d10cb5963f820caac6e947f686a8f4284
MarJC5 <jmartin@student.42lausanne.ch>
2022-01-27 09:56:39 +0100

    Update

---
+++ free_disk.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+rm -rf ~/Library/caches/firefox/* -y
+rm -rf ~/Library/caches/vscode-cpptools/* -y
+rm -rf ~/Library/caches/com.tinyspeck.slackmacgap/* -y
+rm -rf ~/Library/caches/com.google.SoftwareUpdate/* -y
+rm -rf ~/Library/caches/com.apple.Safari/* -y
+rm -rf ~/Library/caches/Google/* -y
+rm -rf ~/Library/caches/com.apple.Safari.SafeBrowsing/* -y
+rm -rf ~/Library/caches/com.hnc.Discord/* -y
+if [ -d ~/.Trash ]; then
+	cd ~/.Trash | rm -rv ~/.Trash
+fi
